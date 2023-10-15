38852002cf401fbd23539c90a0d53d4965ec6050
Thapaswini Konka <146010938+ThapaswiniKonka@users.noreply.github.com>
2023-10-15 15:20:08 +0530

    Update Github.py

--- Github.py
+++ Github.py
@@ -1,9 +1,9 @@
 print("CHECKOUT THAT IF YOU ARE ELIGIBLE FOR B.TECH AFTER DIPLOMA OR NOT")
-credits1=int(input("enter credits in 1st sem:"))
-credits2=int(input("enter credits in 2nd sem:"))
-credits3=int(input("enter credits in 3rd sem3:"))
-credits4=int(input("enter credits in 4th sem4:"))
-credits5=int(input("enter credits in 5th sem5:"))
+credits1=int(input("Enter credits in 1st sem:"))
+credits2=int(input("Enter credits in 2nd sem:"))
+credits3=int(input("Enter credits in 3rd sem3:"))
+credits4=int(input("Enter credits in 4th sem4:"))
+credits5=int(input("Enter credits in 5th sem5:"))
 sum=credits1+credits2+credits3+credits4+credits5
 print("TOTAL CREDITS:", sum)
 if sum>150:
